de0dafa400730b3dc7f1d1d2a3a43adfff1585b0
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-24 01:49:59 +0100

    bothworks

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import * as SimpleUsersController from './controllers/nestedUsersController.js';
+import * as SimpleUsersController from './controllers/simpleUsersController.js';
 import * as NestedUsersController from './controllers/nestedUsersController.js';
 import mongoose from 'mongoose';
 
