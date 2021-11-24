e3f3bb6568c7d6caea1dfd5bac0721061187b47b
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-24 01:29:01 +0100

    crud in order

--- server.js
+++ server.js
@@ -9,12 +9,7 @@ const port = 3033;
 
 app.use(express.json());
 
-app.get('/simpleUsers', async (req, res) => {
-	res.json({
-		simpleUsers: await SimpleUsersController.getAllSimpleUsers()
-	});
-});
-
+// simple users: CREATE
 app.post('/simpleUsers/create', async (req, res) => {
 	const simpleUserObj = req.body;
 	const result = await SimpleUsersController.createSimpleUser(simpleUserObj);
@@ -23,6 +18,14 @@ app.post('/simpleUsers/create', async (req, res) => {
 	});
 });
 
+// simple users: READ
+app.get('/simpleUsers', async (req, res) => {
+	res.json({
+		simpleUsers: await SimpleUsersController.getAllSimpleUsers()
+	});
+});
+
+// simple users: DELETE
 app.post('/simpleUsers/delete/:id', async (req, res) => {
 	const id = req.params.id;
 	const result = await SimpleUsersController.deleteSimpleUser(id);
@@ -31,6 +34,7 @@ app.post('/simpleUsers/delete/:id', async (req, res) => {
 	});
 });
 
+// simple users: UPDATE
 app.patch('/simpleUsers/update/:id', async (req, res) => {
 	const id = req.params.id;
 	const updateFields = req.body
